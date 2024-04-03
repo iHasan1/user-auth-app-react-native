27784d6b60bda07804f0ed3dcf2a2bb6b2289aaa
Imran Hasan <imran.h94@gmail.com>
2024-04-04 01:56:03 +0500

    added error handling logic as well logic to use the token returned from firebase inside the code by storing it AuthContext

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 import { login } from "../util/auth";
 
 function LoginScreen() {
-  const [isAuthenticating, setIsAuthenticating] = useState();
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed!", "Please check your credentials");
+    }
     setIsAuthenticating(false);
   }
 
--- util/auth.js
+++ util/auth.js
@@ -6,23 +6,21 @@ const { apiKey } = Constants.expoConfig.extra;
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${apiKey}`;
 
-  try {
-    const response = await axios.post(url, {
-      email,
-      password,
-      returnSecureToken: true,
-    });
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
 
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
+  const token = response.data.idToken;
+
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
